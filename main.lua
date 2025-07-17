a8ca049f358942beda6b94f13c0dcf162cdcd882
Myland1 <xnot.milanx@gmail.com>
2025-07-17 23:50:51 +0200

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,46 @@
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+
+local Window = Rayfield:CreateWindow({
+    Name = "Mijn Script GUI",
+    LoadingTitle = "Mijn Script Laadt...",
+    LoadingSubtitle = "Door Milan",
+    ConfigurationSaving = {
+       Enabled = true,
+       FolderName = nil, -- of vul een mapnaam in
+       FileName = "MijnScriptConfig"
+    },
+    Discord = {
+       Enabled = false,
+       Invite = "", -- Vul je Discord invite in als je wilt
+       RememberJoins = true
+    },
+    KeySystem = false, -- Zet op true als je een key wilt gebruiken
+    KeySettings = {
+       Title = "Key Systeem",
+       Subtitle = "Key Systeem",
+       Note = "Vraag de key aan de eigenaar",
+       FileName = "Key", -- Sla de key lokaal op
+       SaveKey = true,
+       GrabKeyFromSite = false,
+       Key = "JOUWKEYHIER"
+    }
+})
+
+local MainTab = Window:CreateTab("Hoofd", 4483362458) -- Tab met een icoon
+
+MainTab:CreateButton({
+    Name = "Klik mij!",
+    Callback = function()
+        print("Knop is geklikt!")
+        -- Hier kun je je eigen code zetten
+    end,
+})
+
+MainTab:CreateToggle({
+    Name = "Auto Farm",
+    CurrentValue = false,
+    Callback = function(Value)
+        print("Auto Farm aan/uit:", Value)
+        -- Hier kun je je auto-farm code starten/stoppen
+    end,
+})
