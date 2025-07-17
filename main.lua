18bd1fb4f7fe1af15a0af116f5cbbd74910a6389
Myland1 <xnot.milanx@gmail.com>
2025-07-18 00:47:33 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,7 +3,16 @@ local Window = Library.CreateLib("TITLE", "DarkTheme")
 
 local Tab = Window:NewTab("AutoBuy")
 local Section = Tab:NewSection("Seed Shop")
+Section:NewDropdown("DropdownText", "DropdownInf", {"Option 1", "Option 2", "Option 3"}, function(currentOption)
+    print(currentOption)
+end)
 
 local Section = Tab:NewSection("Gear Shop")
+Section:NewDropdown("DropdownText", "DropdownInf", {"Option 1", "Option 2", "Option 3"}, function(currentOption)
+    print(currentOption)
+end)
 
 local Section = Tab:NewSection("Egg Shop")
+Section:NewDropdown("DropdownText", "DropdownInf", {"Option 1", "Option 2", "Option 3"}, function(currentOption)
+    print(currentOption)
+end)
